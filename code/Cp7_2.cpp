df33bbe8fa99b565738591fddef5086067d84dc5
mengjiyong <1020225147@qq.com>
2021-02-23 22:46:09 +0800

    delete unnecessary files

--- code/Cp7_2.cpp
+++
@@ -1,8 +0,0 @@
-/*
-	7.2 函数参数和按值传递
-	1.接收值的变量称为形参，传递给函数的值被称为实参
-*/
-
-#include<iostream>
-using namespace std;
-
--- code/Cp9_2_5_a.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int harry = 300;
-
-void func1()
-{
-	cout << "harry: " <<harry<< endl;
-}
